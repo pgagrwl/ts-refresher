76ad89b6acb68a92d71840893c84d3a562dbe511
pgagrwl <pgagrwl@gmail.com>
2023-12-18 10:47:38 +0530

    Enums

--- src/Enums.ts
+++ src/Enums.ts
@@ -2,4 +2,32 @@ enum AuthError {
   WRONG_CREDENTIALS,
   SERVER_FAIL,
   EXPIRED_SESSION,
+  UNEXPECTED_ERROR,
 }
+
+console.log(AuthError[AuthError.WRONG_CREDENTIALS]);
+
+enum AuthError2 {
+  WRONG_CREDENTIALS = "wrong credentials",
+  SERVER_FAIL = "server fail",
+  EXPIRED_SESSION = "expired session",
+}
+
+function handleError(error: AuthError) {
+  switch (error) {
+    case AuthError.SERVER_FAIL:
+      console.log("Restart the server");
+      break;
+    case AuthError.EXPIRED_SESSION:
+      console.log("Get a new session");
+      break;
+    case AuthError.WRONG_CREDENTIALS:
+    case AuthError.UNEXPECTED_ERROR:
+      console.log("Check the input");
+      break;
+    default:
+      break;
+  }
+}
+
+handleError(AuthError.UNEXPECTED_ERROR);
